908ece09212b27b01767da4c3ea0206fd4ba80d0
Myeongjong Kim <jjong8942@gmail.com>
2024-12-04 23:12:53 +0100

    Create template in navigation bar added

---
+++ src/main/java/com/devteam45ldm/ldm/views/createtemplate/CreateTemplate.java
@@ -0,0 +1,69 @@
+package com.devteam45ldm.ldm.views.createtemplate;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
+import java.util.ArrayList;
+import java.util.List;
+
+@PageTitle("Create Template")
+@Route("my-view")
+@Menu(order = 2, icon = "line-awesome/svg/pencil-ruler-solid.svg")
+public class CreateTemplate extends Composite<VerticalLayout> {
+
+
+    public CreateTemplate() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        Select select = new Select();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow);
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.addClassName(Padding.XSMALL);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutRow.setAlignItems(Alignment.START);
+        layoutRow.setJustifyContentMode(JustifyContentMode.END);
+        layoutRow2.setHeightFull();
+        layoutRow.setFlexGrow(1.0, layoutRow2);
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.addClassName(Padding.XSMALL);
+        layoutRow2.setWidth("100%");
+        layoutRow2.getStyle().set("flex-grow", "1");
+        select.setLabel("Select Your Template");
+        select.setWidth("min-content");
+        setSelectSampleData(select);
+        buttonSecondary.setText("Create Template");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutRow);
+        layoutRow.add(layoutRow2);
+        layoutRow2.add(select);
+        layoutRow.add(buttonSecondary);
+    }
+
+    record SampleItem(String value, String label, Boolean disabled) {
+    }
+
+    private void setSelectSampleData(Select select) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem("first", "v1", null));
+        sampleItems.add(new SampleItem("second", "v2", null));
+        sampleItems.add(new SampleItem("third", "v3", null));
+        sampleItems.add(new SampleItem("fourth", "v4", null));
+        select.setItems(sampleItems);
+        select.setItemLabelGenerator(item -> ((SampleItem) item).label());
+        select.setItemEnabledProvider(item -> !Boolean.TRUE.equals(((SampleItem) item).disabled()));
+    }
+}
