7377948e7c87c7233042801ace525fe0e5a85044
Myeongjong Kim <jjong8942@gmail.com>
2024-12-18 18:28:47 +0100

    MongoDB dependencies updated

--- src/main/java/com/devteam45ldm/ldm/config/MongoConfig.java
+++ src/main/java/com/devteam45ldm/ldm/config/MongoConfig.java
@@ -1,16 +1,10 @@
 package com.devteam45ldm.ldm.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.devteam45ldm.ldm")
-public class MongoConfig extends AbstractMongoClientConfiguration {
-
-    @Override
-    protected String getDatabaseName() {
-        return "ldm-database";
-    }
+public class MongoConfig {
 }
 
