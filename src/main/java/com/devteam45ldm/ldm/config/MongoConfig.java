4fdb2aae0b4b8fc7b7b79b129a9415509a52f383
Myeongjong Kim <jjong8942@gmail.com>
2024-12-18 18:07:17 +0100

    MongoDB configuration file fixed

--- src/main/java/com/devteam45ldm/ldm/config/MongoConfig.java
+++ src/main/java/com/devteam45ldm/ldm/config/MongoConfig.java
@@ -1,21 +1,16 @@
 package com.devteam45ldm.ldm.config;
 
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-
-public class MongoConfig {
-    package com.devteam45ldm.ldm.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-    @Configuration
-    @EnableMongoRepositories(basePackages = "com.devteam45ldm.ldm")
-    public class MongoConfig extends AbstractMongoClientConfiguration {
+@Configuration
+@EnableMongoRepositories(basePackages = "com.devteam45ldm.ldm")
+public class MongoConfig extends AbstractMongoClientConfiguration {
 
-        @Override
-        protected String getDatabaseName() {
-            return "ldm-database";
-        }
+    @Override
+    protected String getDatabaseName() {
+        return "ldm-database";
     }
 }
+
