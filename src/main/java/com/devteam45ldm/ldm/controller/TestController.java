113db5efdc781a559df9651888695644c92d7fc0
WernersGit <124093481+WernersGit@users.noreply.github.com>
2024-12-18 20:26:35 +0100

    implemented a vaadin2mongodb connection test

---
+++ src/main/java/com/devteam45ldm/ldm/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.devteam45ldm.ldm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @GetMapping("/test-mongo-connection")
+    public ResponseEntity<String> testMongoConnection() {
+        boolean collectionExists = mongoTemplate.collectionExists("admin");
+        if (collectionExists) {
+            return ResponseEntity.ok("MongoDB connection is successful.");
+        } else {
+            return ResponseEntity.status(500).body("MongoDB connection failed.");
+        }
+    }
+}
