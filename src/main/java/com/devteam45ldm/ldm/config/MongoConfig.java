7917a2ea2adbdf4688643c4eb51fb22287edc2da
Myeongjong Kim <jjong8942@gmail.com>
2024-12-18 18:05:37 +0100

    MongoDB configuration file added

---
+++ src/main/java/com/devteam45ldm/ldm/config/MongoConfig.java
@@ -0,0 +1,21 @@
+package com.devteam45ldm.ldm.config;
+
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+public class MongoConfig {
+    package com.devteam45ldm.ldm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+    @Configuration
+    @EnableMongoRepositories(basePackages = "com.devteam45ldm.ldm")
+    public class MongoConfig extends AbstractMongoClientConfiguration {
+
+        @Override
+        protected String getDatabaseName() {
+            return "ldm-database";
+        }
+    }
+}
