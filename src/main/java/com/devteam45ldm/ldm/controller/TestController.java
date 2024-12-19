aa2df98e515aeb33e4a88a6107735910c8fb306d
WernersGit <124093481+WernersGit@users.noreply.github.com>
2024-12-19 12:11:47 +0100

    added vaadin2vault connection and connection test

--- src/main/java/com/devteam45ldm/ldm/controller/TestController.java
+++ src/main/java/com/devteam45ldm/ldm/controller/TestController.java
@@ -2,6 +2,7 @@ package com.devteam45ldm.ldm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.vault.core.VaultTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,25 @@ public class TestController {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private VaultTemplate vaultTemplate;
+
     @GetMapping("/test/mongo-connection")
     public ResponseEntity<String> testMongoConnection() {
         if (mongoTemplate.getDb().getName().equals("ldm")) {
             return ResponseEntity.ok("MongoDB connection is successful.");
         } else {
             return ResponseEntity.status(500).body("MongoDB connection failed.");
+        }
+    }
+
+    @GetMapping("/test/vault-connection")
+    public ResponseEntity<String> testVaultConnection() {
+        try {
+            vaultTemplate.opsForSys().health();
+            return ResponseEntity.ok("Hashicorp Vault connection is successful.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Hashicorp Vault connection failed: " + e.getMessage());
         }
     }
 }
