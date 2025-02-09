a4618979b21b98a8493535d51e0bd12795270958
WernersGit <124093481+WernersGit@users.noreply.github.com>
2025-02-09 02:47:57 +0100

    added MetadataTest and Docs

--- src/main/java/com/devteam45ldm/ldm/parser/templates/Metadata.java
+++ src/main/java/com/devteam45ldm/ldm/parser/templates/Metadata.java
@@ -27,95 +27,207 @@ public class Metadata {
     private Parser.ParserType parser = null;
 
 
+    /**
+     * Gets the version of the metadata.
+     *
+     * @return the version of the metadata
+     */
     public Integer getVersion() {
         return version;
     }
 
+    /**
+     * Sets the version of the metadata.
+     *
+     * @param version the version to set
+     */
     public void setVersion(Integer version) {
         this.version = version;
     }
 
+    /**
+     * Sets the version of the metadata and returns the updated Metadata object.
+     *
+     * @param version the version to set
+     * @return the updated Metadata object
+     */
     public Metadata version(Integer version) {
         this.version = version;
         return this;
     }
 
+    /**
+     * Gets the creator of the metadata.
+     *
+     * @return the creator of the metadata
+     */
     public String getCreatedBy() {
         return createdBy;
     }
 
+    /**
+     * Sets the creator of the metadata.
+     *
+     * @param createdBy the creator to set
+     */
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
+    /**
+     * Sets the creator of the metadata and returns the updated Metadata object.
+     *
+     * @param createdBy the creator to set
+     * @return the updated Metadata object
+     */
     public Metadata createdBy(String createdBy) {
         this.createdBy = createdBy;
         return this;
     }
 
+    /**
+     * Gets the creation date of the metadata.
+     *
+     * @return the creation date of the metadata
+     */
     public String getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Sets the creation date of the metadata.
+     *
+     * @param createdAt the creation date to set
+     */
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Sets the creation date of the metadata and returns the updated Metadata object.
+     *
+     * @param createdAt the creation date to set
+     * @return the updated Metadata object
+     */
     public Metadata createdAt(String createdAt) {
         this.createdAt = createdAt;
         return this;
     }
 
+    /**
+     * Gets the last modifier of the metadata.
+     *
+     * @return the last modifier of the metadata
+     */
     public String getLastModifiedBy() {
         return lastModifiedBy;
     }
 
+    /**
+     * Sets the last modifier of the metadata.
+     *
+     * @param lastModifiedBy the last modifier to set
+     */
     public void setLastModifiedBy(String lastModifiedBy) {
         this.lastModifiedBy = lastModifiedBy;
     }
 
+    /**
+     * Sets the last modifier of the metadata and returns the updated Metadata object.
+     *
+     * @param lastModifiedBy the last modifier to set
+     * @return the updated Metadata object
+     */
     public Metadata lastModifiedBy(String lastModifiedBy) {
         this.lastModifiedBy = lastModifiedBy;
         return this;
     }
 
+    /**
+     * Gets the last modification date of the metadata.
+     *
+     * @return the last modification date of the metadata
+     */
     public String getLastModifiedAt() {
         return lastModifiedAt;
     }
 
+    /**
+     * Sets the last modification date of the metadata.
+     *
+     * @param lastModifiedAt the last modification date to set
+     */
     public void setLastModifiedAt(String lastModifiedAt) {
         this.lastModifiedAt = lastModifiedAt;
     }
 
+    /**
+     * Sets the last modification date of the metadata and returns the updated Metadata object.
+     *
+     * @param lastModifiedAt the last modification date to set
+     * @return the updated Metadata object
+     */
     public Metadata lastModifiedAt(String lastModifiedAt) {
         this.lastModifiedAt = lastModifiedAt;
         return this;
     }
 
+    /**
+     * Gets the parser type of the metadata.
+     *
+     * @return the parser type of the metadata
+     */
     public Parser.ParserType getParser() {
         return parser;
     }
 
+    /**
+     * Gets the parser type of the metadata.
+     *
+     * @return the parser type of the metadata
+     */
     public void setParser(Parser.ParserType parser) {
         this.parser = parser;
     }
 
+    /**
+     * Sets the parser type of the metadata and returns the updated Metadata object.
+     *
+     * @param parser the parser type to set
+     * @return the updated Metadata object
+     */
     public Metadata parser(Parser.ParserType parser) {
         this.parser = parser;
         return this;
     }
 
+    /**
+     * Checks if this metadata is equal to another object.
+     *
+     * @param o the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Metadata metadata)) return false;
         return Objects.equals(getVersion(), metadata.getVersion()) && Objects.equals(getCreatedBy(), metadata.getCreatedBy()) && Objects.equals(getCreatedAt(), metadata.getCreatedAt()) && Objects.equals(getLastModifiedBy(), metadata.getLastModifiedBy()) && Objects.equals(getLastModifiedAt(), metadata.getLastModifiedAt()) && getParser() == metadata.getParser();
     }
 
+    /**
+     * Returns the hash code of this metadata.
+     *
+     * @return the hash code of this metadata
+     */
     @Override
     public int hashCode() {
         return Objects.hash(version, createdBy, createdAt, lastModifiedBy, lastModifiedAt, parser);
     }
 
+    /**
+     * Returns the string representation of this metadata.
+     *
+     * @return the string representation of this metadata
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
@@ -130,8 +242,11 @@ public class Metadata {
     }
 
     /**
-     * Convert the given object to string with each line indented by 4 spaces
+     * Converts the given object to string with each line indented by 4 spaces
      * (except the first line).
+     *
+     * @param o the object to convert
+     * @return the string representation of the object
      */
     private String toIndentedString(java.lang.Object o) {
         if (o == null) {
---
+++ src/test/java/com/devteam45ldm/ldm/parser/templates/MetadataTest.java
@@ -0,0 +1,224 @@
+package com.devteam45ldm.ldm.parser.templates;
+
+import com.devteam45ldm.ldm.parser.Parser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetadataTest {
+
+    /**
+     * Tests if getVersion() returns the correct version.
+     */
+    @Test
+    void getVersion_returnsCorrectVersion() {
+        Metadata metadata = new Metadata();
+        metadata.setVersion(1);
+        assertEquals(1, metadata.getVersion());
+    }
+
+    /**
+     * Tests if setVersion() sets the correct version.
+     */
+    @Test
+    void setVersion_setsCorrectVersion() {
+        Metadata metadata = new Metadata();
+        metadata.setVersion(2);
+        assertEquals(2, metadata.getVersion());
+    }
+
+    /**
+     * Tests if getCreatedBy() returns the correct creator.
+     */
+    @Test
+    void getCreatedBy_returnsCorrectCreatedBy() {
+        Metadata metadata = new Metadata();
+        metadata.setCreatedBy("user");
+        assertEquals("user", metadata.getCreatedBy());
+    }
+
+    /**
+     * Tests if setCreatedBy() sets the correct creator.
+     */
+    @Test
+    void setCreatedBy_setsCorrectCreatedBy() {
+        Metadata metadata = new Metadata();
+        metadata.setCreatedBy("admin");
+        assertEquals("admin", metadata.getCreatedBy());
+    }
+
+    /**
+     * Tests if getCreatedAt() returns the correct creation date.
+     */
+    @Test
+    void getCreatedAt_returnsCorrectCreatedAt() {
+        Metadata metadata = new Metadata();
+        metadata.setCreatedAt("2023-10-01");
+        assertEquals("2023-10-01", metadata.getCreatedAt());
+    }
+
+    /**
+     * Tests if setCreatedAt() sets the correct creation date.
+     */
+    @Test
+    void setCreatedAt_setsCorrectCreatedAt() {
+        Metadata metadata = new Metadata();
+        metadata.setCreatedAt("2023-11-01");
+        assertEquals("2023-11-01", metadata.getCreatedAt());
+    }
+
+    /**
+     * Tests if getLastModifiedBy() returns the correct last modifier.
+     */
+    @Test
+    void getLastModifiedBy_returnsCorrectLastModifiedBy() {
+        Metadata metadata = new Metadata();
+        metadata.setLastModifiedBy("editor");
+        assertEquals("editor", metadata.getLastModifiedBy());
+    }
+
+    /**
+     * Tests if setLastModifiedBy() sets the correct last modifier.
+     */
+    @Test
+    void setLastModifiedBy_setsCorrectLastModifiedBy() {
+        Metadata metadata = new Metadata();
+        metadata.setLastModifiedBy("reviewer");
+        assertEquals("reviewer", metadata.getLastModifiedBy());
+    }
+
+    /**
+     * Tests if getLastModifiedAt() returns the correct last modification date.
+     */
+    @Test
+    void getLastModifiedAt_returnsCorrectLastModifiedAt() {
+        Metadata metadata = new Metadata();
+        metadata.setLastModifiedAt("2023-12-01");
+        assertEquals("2023-12-01", metadata.getLastModifiedAt());
+    }
+
+    /**
+     * Tests if setLastModifiedAt() sets the correct last modification date.
+     */
+    @Test
+    void setLastModifiedAt_setsCorrectLastModifiedAt() {
+        Metadata metadata = new Metadata();
+        metadata.setLastModifiedAt("2024-01-01");
+        assertEquals("2024-01-01", metadata.getLastModifiedAt());
+    }
+
+    /**
+     * Tests if getParser() returns the correct parser type.
+     */
+    @Test
+    void getParser_returnsCorrectParser() {
+        Metadata metadata = new Metadata();
+        metadata.setParser(Parser.ParserType.JSON2JSON);
+        assertEquals(Parser.ParserType.JSON2JSON, metadata.getParser());
+    }
+
+    /**
+     * Tests if setParser() sets the correct parser type.
+     */
+    @Test
+    void setParser_setsCorrectParser() {
+        Metadata metadata = new Metadata();
+        metadata.setParser(Parser.ParserType.XML2JSON);
+        assertEquals(Parser.ParserType.XML2JSON, metadata.getParser());
+    }
+
+    /**
+     * Tests if equals() returns true for the same object.
+     */
+    @Test
+    void equals_sameObject_returnsTrue() {
+        Metadata metadata = new Metadata();
+        assertTrue(metadata.equals(metadata));
+    }
+
+    /**
+     * Tests if equals() returns false for different objects.
+     */
+    @Test
+    void equals_differentObject_returnsFalse() {
+        Metadata metadata1 = new Metadata();
+        Metadata metadata2 = new Metadata();
+        assertTrue(metadata1.equals(metadata2));
+    }
+
+    /**
+     * Tests if hashCode() returns the same hash code for the same object.
+     */
+    @Test
+    void hashCode_sameObject_returnsSameHashCode() {
+        Metadata metadata = new Metadata();
+        assertEquals(metadata.hashCode(), metadata.hashCode());
+    }
+
+    /**
+     * Tests if toString() returns the correct string representation.
+     */
+    @Test
+    void toString_returnsCorrectStringRepresentation() {
+        Metadata metadata = new Metadata();
+        metadata.setVersion(1);
+        metadata.setCreatedBy("user");
+        metadata.setCreatedAt("2023-10-01");
+        metadata.setLastModifiedBy("editor");
+        metadata.setLastModifiedAt("2023-12-01");
+        metadata.setParser(Parser.ParserType.JSON2JSON);
+        String expected = "{\n" +
+                "    version: 1\n" +
+                "    created_by: user\n" +
+                "    created_at: 2023-10-01\n" +
+                "    last_modified_by: editor\n" +
+                "    last_modified_at: 2023-12-01\n" +
+                "    parser: JSON2JSON\n" +
+                "}";
+        assertEquals(expected, metadata.toString());
+    }
+
+    /**
+     * Tests if a Metadata object is correctly created from a JSON string.
+     */
+    @Test
+    void fromJsonString_createsMetadataObject() throws Exception {
+        String json = "{ \"version\": 1, \"created_by\": \"user\", \"created_at\": \"2023-10-01\", \"last_modified_by\": \"editor\", \"last_modified_at\": \"2023-12-01\", \"parser\": \"json_2_json\" }";
+        ObjectMapper objectMapper = new ObjectMapper();
+        Metadata metadata = objectMapper.readValue(json, Metadata.class);
+
+        assertEquals(1, metadata.getVersion());
+        assertEquals("user", metadata.getCreatedBy());
+        assertEquals("2023-10-01", metadata.getCreatedAt());
+        assertEquals("editor", metadata.getLastModifiedBy());
+        assertEquals("2023-12-01", metadata.getLastModifiedAt());
+        assertEquals(Parser.ParserType.JSON2JSON, metadata.getParser());
+    }
+
+    /**
+     * Tests if a Metadata object with null fields is correctly created from a JSON string with missing fields.
+     */
+    @Test
+    void fromJsonString_missingFields_createsMetadataObjectWithNulls() throws Exception {
+        String json = "{ \"version\": 1, \"created_by\": \"user\" }";
+        ObjectMapper objectMapper = new ObjectMapper();
+        Metadata metadata = objectMapper.readValue(json, Metadata.class);
+
+        assertEquals(1, metadata.getVersion());
+        assertEquals("user", metadata.getCreatedBy());
+        assertNull(metadata.getCreatedAt());
+        assertNull(metadata.getLastModifiedBy());
+        assertNull(metadata.getLastModifiedAt());
+        assertNull(metadata.getParser());
+    }
+
+    /**
+     * Tests if an exception is thrown for an invalid JSON string.
+     */
+    @Test
+    void fromJsonString_invalidJson_throwsException() {
+        String json = "{ \"version\": 1, \"created_by\": \"user\", ";
+        ObjectMapper objectMapper = new ObjectMapper();
+        assertThrows(Exception.class, () -> objectMapper.readValue(json, Metadata.class));
+    }
+}
