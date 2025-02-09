100b7eeb50968800d176a2e200263a6dfdac8142
WernersGit <124093481+WernersGit@users.noreply.github.com>
2025-02-09 03:03:47 +0100

    added ImportMappingsTest and Docs

--- src/main/java/com/devteam45ldm/ldm/parser/templates/importDataStructures/ImportMappings.java
+++ src/main/java/com/devteam45ldm/ldm/parser/templates/importDataStructures/ImportMappings.java
@@ -24,97 +24,207 @@ public class ImportMappings {
     @JsonProperty("data_pattern")
     private Pattern dataPattern = null;
 
-
+    /**
+     * Gets the metadata.
+     *
+     * @return the metadata
+     */
     public String getMetadata() {
         return metadata;
     }
 
+    /**
+     * Sets the metadata.
+     *
+     * @param metadata the metadata to set
+     */
     public void setMetadata(String metadata) {
         this.metadata = metadata;
     }
 
+    /**
+     * Sets the metadata and returns the current ImportMappings object.
+     *
+     * @param metadata the metadata to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings metadata(String metadata) {
         this.metadata = metadata;
         return this;
     }
 
+    /**
+     * Gets the metadata separator.
+     *
+     * @return the metadata separator
+     */
     public String getMetadataSeparator() {
         return metadataSeparator;
     }
 
+    /**
+     * Sets the metadata separator.
+     *
+     * @param metadataSeparator the metadata separator to set
+     */
     public void setMetadataSeparator(String metadataSeparator) {
         this.metadataSeparator = metadataSeparator;
     }
 
+    /**
+     * Sets the metadata separator and returns the current ImportMappings object.
+     *
+     * @param metadataSeparator the metadata separator to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings metadataSeparator(String metadataSeparator) {
         this.metadataSeparator = metadataSeparator;
         return this;
     }
 
+    /**
+     * Gets the metadata pattern.
+     *
+     * @return the metadata pattern
+     */
     public Pattern getMetadataPattern() {
         return metadataPattern;
     }
 
+    /**
+     * Sets the metadata pattern.
+     *
+     * @param metadataPattern the metadata pattern to set
+     */
     public void setMetadataPattern(Pattern metadataPattern) {
         this.metadataPattern = metadataPattern;
     }
 
+    /**
+     * Sets the metadata pattern and returns the current ImportMappings object.
+     *
+     * @param metadataPattern the metadata pattern to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings metadataPattern(Pattern metadataPattern) {
         this.metadataPattern = metadataPattern;
         return this;
     }
 
+    /**
+     * Gets the data.
+     *
+     * @return the data
+     */
     public String getData() {
         return data;
     }
 
+    /**
+     * Sets the data.
+     *
+     * @param data the data to set
+     */
     public void setData(String data) {
         this.data = data;
     }
 
+    /**
+     * Sets the data and returns the current ImportMappings object.
+     *
+     * @param data the data to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings data(String data) {
         this.data = data;
         return this;
     }
 
+    /**
+     * Gets the data separator.
+     *
+     * @return the data separator
+     */
     public String getDataSeparator() {
         return dataSeparator;
     }
 
+    /**
+     * Sets the data separator.
+     *
+     * @param dataSeparator the data separator to set
+     */
     public void setDataSeparator(String dataSeparator) {
         this.dataSeparator = dataSeparator;
     }
 
+    /**
+     * Sets the data separator and returns the current ImportMappings object.
+     *
+     * @param dataSeparator the data separator to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings dataSeparator(String dataSeparator) {
         this.dataSeparator = dataSeparator;
         return this;
     }
 
+    /**
+     * Gets the data pattern.
+     *
+     * @return the data pattern
+     */
     public Pattern getDataPattern() {
         return dataPattern;
     }
 
+    /**
+     * Sets the data pattern.
+     *
+     * @param dataPattern the data pattern to set
+     */
     public void setDataPattern(Pattern dataPattern) {
         this.dataPattern = dataPattern;
     }
 
+    /**
+     * Sets the data pattern and returns the current ImportMappings object.
+     *
+     * @param dataPattern the data pattern to set
+     * @return the current ImportMappings object
+     */
     public ImportMappings dataPattern(Pattern dataPattern) {
         this.dataPattern = dataPattern;
         return this;
     }
 
-
+    /**
+     * Checks if this ImportMappings object is equal to another object.
+     *
+     * @param o the object to compare
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof ImportMappings importMappings)) return false;
         return Objects.equals(getMetadata(), importMappings.getMetadata()) && Objects.equals(getMetadataSeparator(), importMappings.getMetadataSeparator()) && Objects.equals(getMetadataPattern(), importMappings.getMetadataPattern()) && Objects.equals(getData(), importMappings.getData()) && Objects.equals(getDataSeparator(), importMappings.getDataSeparator()) && Objects.equals(getDataPattern(), importMappings.getDataPattern());
     }
 
+    /**
+     * Returns the hash code of this ImportMappings object.
+     *
+     * @return the hash code
+     */
     @Override
     public int hashCode() {
         return Objects.hash(metadata, metadataSeparator, metadataPattern, data, dataSeparator, dataPattern);
     }
 
+    /**
+     * Returns the string representation of this ImportMappings object.
+     *
+     * @return the string representation
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
@@ -129,8 +239,10 @@ public class ImportMappings {
     }
 
     /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
+     * Converts the given object to a string with each line indented by 4 spaces (except the first line).
+     *
+     * @param o the object to convert
+     * @return the string representation of the object
      */
     private String toIndentedString(java.lang.Object o) {
         if (o == null) {
---
+++ src/test/java/com/devteam45ldm/ldm/parser/templates/importDataStructures/ImportMappingsTest.java
@@ -0,0 +1,294 @@
+package com.devteam45ldm.ldm.parser.templates.importDataStructures;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import java.util.regex.Pattern;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImportMappingsTest {
+
+    /**
+     * Tests if getMetadata() returns the correct metadata.
+     */
+    @Test
+    void getMetadata_returnsCorrectMetadata() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setMetadata("metadata");
+        assertEquals("metadata", importMappings.getMetadata());
+    }
+
+    /**
+     * Tests if setMetadata() sets the correct metadata.
+     */
+    @Test
+    void setMetadata_setsCorrectMetadata() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setMetadata("new_metadata");
+        assertEquals("new_metadata", importMappings.getMetadata());
+    }
+
+    /**
+     * Tests if metadata() sets and returns the correct metadata.
+     */
+    @Test
+    void metadata_setsAndReturnsCorrectMetadata() {
+        ImportMappings importMappings = new ImportMappings();
+        assertEquals(importMappings, importMappings.metadata("metadata"));
+        assertEquals("metadata", importMappings.getMetadata());
+    }
+
+    /**
+     * Tests if getMetadataSeparator() returns the correct metadata separator.
+     */
+    @Test
+    void getMetadataSeparator_returnsCorrectMetadataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setMetadataSeparator(",");
+        assertEquals(",", importMappings.getMetadataSeparator());
+    }
+
+    /**
+     * Tests if setMetadataSeparator() sets the correct metadata separator.
+     */
+    @Test
+    void setMetadataSeparator_setsCorrectMetadataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setMetadataSeparator(";");
+        assertEquals(";", importMappings.getMetadataSeparator());
+    }
+
+    /**
+     * Tests if metadataSeparator() sets and returns the correct metadata separator.
+     */
+    @Test
+    void metadataSeparator_setsAndReturnsCorrectMetadataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        assertEquals(importMappings, importMappings.metadataSeparator(","));
+        assertEquals(",", importMappings.getMetadataSeparator());
+    }
+
+    /**
+     * Tests if getMetadataPattern() returns the correct metadata pattern.
+     */
+    @Test
+    void getMetadataPattern_returnsCorrectMetadataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        importMappings.setMetadataPattern(pattern);
+        assertEquals(pattern, importMappings.getMetadataPattern());
+    }
+
+    /**
+     * Tests if setMetadataPattern() sets the correct metadata pattern.
+     */
+    @Test
+    void setMetadataPattern_setsCorrectMetadataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        importMappings.setMetadataPattern(pattern);
+        assertEquals(pattern, importMappings.getMetadataPattern());
+    }
+
+    /**
+     * Tests if metadataPattern() sets and returns the correct metadata pattern.
+     */
+    @Test
+    void metadataPattern_setsAndReturnsCorrectMetadataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        assertEquals(importMappings, importMappings.metadataPattern(pattern));
+        assertEquals(pattern, importMappings.getMetadataPattern());
+    }
+
+    /**
+     * Tests if getData() returns the correct data.
+     */
+    @Test
+    void getData_returnsCorrectData() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setData("data");
+        assertEquals("data", importMappings.getData());
+    }
+
+    /**
+     * Tests if setData() sets the correct data.
+     */
+    @Test
+    void setData_setsCorrectData() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setData("new_data");
+        assertEquals("new_data", importMappings.getData());
+    }
+
+    /**
+     * Tests if data() sets and returns the correct data.
+     */
+    @Test
+    void data_setsAndReturnsCorrectData() {
+        ImportMappings importMappings = new ImportMappings();
+        assertEquals(importMappings, importMappings.data("data"));
+        assertEquals("data", importMappings.getData());
+    }
+
+    /**
+     * Tests if getDataSeparator() returns the correct data separator.
+     */
+    @Test
+    void getDataSeparator_returnsCorrectDataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setDataSeparator(",");
+        assertEquals(",", importMappings.getDataSeparator());
+    }
+
+    /**
+     * Tests if setDataSeparator() sets the correct data separator.
+     */
+    @Test
+    void setDataSeparator_setsCorrectDataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setDataSeparator(";");
+        assertEquals(";", importMappings.getDataSeparator());
+    }
+
+    /**
+     * Tests if dataSeparator() sets and returns the correct data separator.
+     */
+    @Test
+    void dataSeparator_setsAndReturnsCorrectDataSeparator() {
+        ImportMappings importMappings = new ImportMappings();
+        assertEquals(importMappings, importMappings.dataSeparator(","));
+        assertEquals(",", importMappings.getDataSeparator());
+    }
+
+    /**
+     * Tests if getDataPattern() returns the correct data pattern.
+     */
+    @Test
+    void getDataPattern_returnsCorrectDataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        importMappings.setDataPattern(pattern);
+        assertEquals(pattern, importMappings.getDataPattern());
+    }
+
+    /**
+     * Tests if setDataPattern() sets the correct data pattern.
+     */
+    @Test
+    void setDataPattern_setsCorrectDataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        importMappings.setDataPattern(pattern);
+        assertEquals(pattern, importMappings.getDataPattern());
+    }
+
+    /**
+     * Tests if dataPattern() sets and returns the correct data pattern.
+     */
+    @Test
+    void dataPattern_setsAndReturnsCorrectDataPattern() {
+        ImportMappings importMappings = new ImportMappings();
+        Pattern pattern = Pattern.compile(".*");
+        assertEquals(importMappings, importMappings.dataPattern(pattern));
+        assertEquals(pattern, importMappings.getDataPattern());
+    }
+
+    /**
+     * Tests if equals() returns true for the same object.
+     */
+    @Test
+    void equals_sameObject_returnsTrue() {
+        ImportMappings importMappings = new ImportMappings();
+        assertTrue(importMappings.equals(importMappings));
+    }
+
+    /**
+     * Tests if equals() returns false for different objects.
+     */
+    @Test
+    void equals_differentObject_returnsFalse() {
+        ImportMappings importMappings1 = new ImportMappings().data("data");
+        ImportMappings importMappings2 = new ImportMappings();
+        assertFalse(importMappings1.equals(importMappings2));
+    }
+
+    /**
+     * Tests if equals() returns true for objects with the same values.
+     */
+    @Test
+    void equals_sameValues_returnsTrue() {
+        ImportMappings importMappings1 = new ImportMappings();
+        importMappings1.setMetadata("metadata");
+        importMappings1.setData("data");
+
+        ImportMappings importMappings2 = new ImportMappings();
+        importMappings2.setMetadata("metadata");
+        importMappings2.setData("data");
+
+        assertTrue(importMappings1.equals(importMappings2));
+    }
+
+    /**
+     * Tests if hashCode() returns the same hash code for the same object.
+     */
+    @Test
+    void hashCode_sameObject_returnsSameHashCode() {
+        ImportMappings importMappings = new ImportMappings();
+        assertEquals(importMappings.hashCode(), importMappings.hashCode());
+    }
+
+    /**
+     * Tests if toString() returns the correct string representation.
+     */
+    @Test
+    void toString_returnsCorrectStringRepresentation() {
+        ImportMappings importMappings = new ImportMappings();
+        importMappings.setMetadata("metadata");
+        importMappings.setData("data");
+        String expected = "{\n" +
+                "    metadata: metadata\n" +
+                "    metadata_separator: null\n" +
+                "    metadata_pattern: null\n" +
+                "    data: data\n" +
+                "    data_separator: null\n" +
+                "    data_pattern: null\n" +
+                "}";
+        assertEquals(expected, importMappings.toString());
+    }
+
+    /**
+     * Tests if an ImportMappings object is correctly created from a JSON string.
+     */
+    @Test
+    void fromJsonString_createsImportMappingsObject() throws Exception {
+        String json = "{ \"metadata\": \"metadata_value\", \"data\": \"data_value\" }";
+        ObjectMapper objectMapper = new ObjectMapper();
+        ImportMappings importMappings = objectMapper.readValue(json, ImportMappings.class);
+
+        assertEquals("metadata_value", importMappings.getMetadata());
+        assertEquals("data_value", importMappings.getData());
+    }
+
+    /**
+     * Tests if an ImportMappings object with null fields is correctly created from a JSON string with missing fields.
+     */
+    @Test
+    void fromJsonString_missingFields_createsImportMappingsObjectWithNulls() throws Exception {
+        String json = "{ \"metadata\": \"metadata_value\" }";
+        ObjectMapper objectMapper = new ObjectMapper();
+        ImportMappings importMappings = objectMapper.readValue(json, ImportMappings.class);
+
+        assertEquals("metadata_value", importMappings.getMetadata());
+        assertNull(importMappings.getData());
+    }
+
+    /**
+     * Tests if an exception is thrown for an invalid JSON string.
+     */
+    @Test
+    void fromJsonString_invalidJson_throwsException() {
+        String json = "{ \"metadata\": \"metadata_value\", ";
+        ObjectMapper objectMapper = new ObjectMapper();
+        assertThrows(Exception.class, () -> objectMapper.readValue(json, ImportMappings.class));
+    }
+}
