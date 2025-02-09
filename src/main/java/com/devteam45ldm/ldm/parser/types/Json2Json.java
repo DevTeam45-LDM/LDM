ced0e887c596711653170b24634c4d570e1363cb
WernersGit <124093481+WernersGit@users.noreply.github.com>
2025-02-09 19:27:44 +0100

    improving json2json tests

--- src/main/java/com/devteam45ldm/ldm/parser/types/Json2Json.java
+++ src/main/java/com/devteam45ldm/ldm/parser/types/Json2Json.java
@@ -16,64 +16,74 @@ import java.util.Iterator;
 public abstract class Json2Json implements Parser {
 
     /**
-     * Converts the given JSON string into an Import object.
+     * Converts the given JSON string into an ImportedData object based on the ImportTemplate.
      *
      * @param json the JSON string to be converted
-     * @return the resulting Import object
+     * @return the resulting ImportedData object
      * @throws JSONException if there is an error parsing the JSON
      */
     public static ImportedData parse(String json, ImportTemplate importTemplate) throws JSONException {
         JSONObject jsonObject = new JSONObject(json);
         ImportMappings mappings = importTemplate.getData();
 
-        ArrayList<String> metadataPath = mappings.getMetadata();
-        ArrayList<String> dataPath = mappings.getData();
+        JSONObject metadata = extractNestedJson(jsonObject, mappings.getMetadata());
+        JSONObject data = extractNestedJson(jsonObject, mappings.getData());
 
-        String metadata = extractImmediateChildJsonValue(jsonObject, metadataPath);
-        String data = extractImmediateChildJsonValue(jsonObject, dataPath);
+        ImportedData importedData = new ImportedData();
+        importedData.setMetadata(metadata.toString());
+        importedData.setData(data.toString());
 
-        ImportedData anImportedData = new ImportedData();
-        anImportedData.setMetadata(metadata);
-        anImportedData.setData(data);
-
-        return anImportedData;
+        return importedData;
     }
 
     /**
-     * Extracts the immediate child value from the JSON object based on the specified path.
+     * Extracts JSON data based on a list of paths and merges results.
      *
-     * @param jsonObject the JSON object to extract the value from
-     * @param path the path to the value
-     * @return the extracted value as a string
+     * @param jsonObject the source JSON object
+     * @param paths      list of JSON paths to extract data from
+     * @return a combined JSONObject containing all extracted data
      * @throws JSONException if there is an error parsing the JSON
      */
-    private static String extractImmediateChildJsonValue(JSONObject jsonObject, ArrayList<String> path) throws JSONException {
-        JSONObject currentObject = jsonObject;
-        for (String key : path) {
-            if (currentObject.has(key)) {
-                currentObject = currentObject.getJSONObject(key);
-            } else {
-                return "{}";
-            }
+    private static JSONObject extractNestedJson(JSONObject jsonObject, ArrayList<String> paths) throws JSONException {
+        JSONObject result = new JSONObject();
+        for (String path : paths) {
+            JSONObject extracted = extractByPath(jsonObject, path);
+            mergeJson(result, extracted);
         }
-        return getImmediateChildJson(currentObject).toString();
+        return result;
     }
 
     /**
-     * Gets the immediate child JSON object without further nested objects.
+     * Extracts a JSON object based on a dot-separated path.
      *
-     * @param jsonObject the JSON object to extract the immediate children from
-     * @return the immediate child JSON object
+     * @param jsonObject the source JSON object
+     * @param path       the JSON path (e.g., "kopfdaten.details")
+     * @return the extracted JSONObject or an empty object if the path is not found
+     * @throws JSONException if there is an error parsing the JSON
      */
-    private static JSONObject getImmediateChildJson(JSONObject jsonObject) throws JSONException {
-        JSONObject result = new JSONObject();
-        for (Iterator it = jsonObject.keys(); it.hasNext(); ) {
-            String key = it.next().toString();
-            Object value = jsonObject.get(key);
-            if (!(value instanceof JSONObject)) {
-                result.put(key, value);
+    private static JSONObject extractByPath(JSONObject jsonObject, String path) throws JSONException {
+        JSONObject current = jsonObject;
+        String[] keys = path.split("\\.");
+        for (String key : keys) {
+            if (current.has(key) && current.get(key) instanceof JSONObject) {
+                current = current.getJSONObject(key);
+            } else {
+                return new JSONObject();
             }
         }
-        return result;
+        return current;
+    }
+
+    /**
+     * Merges two JSON objects, prioritizing values from the source.
+     *
+     * @param target the target JSON object where data is merged into
+     * @param source the source JSON object to merge from
+     */
+    private static void mergeJson(JSONObject target, JSONObject source) throws JSONException {
+        for (Iterator<String> it = source.keys(); it.hasNext(); ) {
+            String key = it.next();
+            target.put(key, source.get(key));
+        }
     }
 }
