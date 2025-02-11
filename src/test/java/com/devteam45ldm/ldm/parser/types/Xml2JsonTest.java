2f83eaca6517869178a049d61647982cb3cc29fd
Myeongjong Kim <jjong8942@gmail.com>
2025-02-11 14:31:40 +0100

    Xml2JsonTest implemented

--- src/test/java/com/devteam45ldm/ldm/parser/types/Xml2JsonTest.java
+++ src/test/java/com/devteam45ldm/ldm/parser/types/Xml2JsonTest.java
@@ -1,67 +1,103 @@
 package com.devteam45ldm.ldm.parser.types;
 
+import com.devteam45ldm.ldm.parser.templates.Metadata;
+import com.devteam45ldm.ldm.parser.templates.importDataStructures.ImportMappings;
+import com.devteam45ldm.ldm.parser.templates.importDataStructures.ImportTemplate;
+import com.devteam45ldm.ldm.parser.templates.importDataStructures.ImportedData;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //TODO: Implement this test suite and adapt to the new Xml2Json implementation
 class Xml2JsonTest {
-
     private final Boolean debug = false;
 
     @Test
     void parseXMLToJson_withValidXml_returnsJson() throws Exception {
         String xml = "<root><child attr=\"value\">text</child></root>";
-        JSONObject json = Xml2Json.parse(xml);
-        if(debug){
-            System.out.println(json.toString(2));
-        }
-        assertEquals("value", json.getJSONObject("root").getJSONObject("child").getString("attr"));
-        assertEquals("text", json.getJSONObject("root").getJSONObject("child").getString("content"));
-    }
+        String metadataPath = "root.child.attr";
+        String dataPath = "root.child.content";
 
-    @Test
-    void parseXMLToJson_withEmptyXml_returnsEmptyJson() throws Exception {
-        String xml = "<root></root>";
-        JSONObject json = Xml2Json.parse(xml);
-        if(debug){
-            System.out.println(json.toString(2));
-        }
-        assertTrue(json.getJSONObject("root").isNull("root"));
-    }
+        ArrayList<String> metadataPaths = new ArrayList<>();
+        metadataPaths.add(metadataPath);
+        ArrayList<String> dataPaths = new ArrayList<>();
+        dataPaths.add(dataPath);
 
-    @Test
-    void parseXMLToJson_withAttributesOnly_returnsJsonWithAttributes() throws Exception {
-        String xml = "<root attr1=\"value1\" attr2=\"value2\"></root>";
-        JSONObject json = Xml2Json.parse(xml);
-        if(debug){
-            System.out.println(json.toString(2));
+        ImportMappings mappings = new ImportMappings()
+                .metadata(metadataPaths)
+                .data(dataPaths);
+
+        ImportTemplate importTemplate = new ImportTemplate()
+                .metadata(new Metadata())
+                .data(mappings);
+
+        ImportedData result = Xml2Json.parse(xml, importTemplate);
+
+        if(debug) {
+            System.out.println("Result: " + result.toString());
         }
-        assertEquals("value1", json.getJSONObject("root").getString("attr1"));
-        assertEquals("value2", json.getJSONObject("root").getString("attr2"));
+
+        JSONObject metadata = new JSONObject(result.getMetadata());
+        assertEquals("value", metadata.getString("attr"));
+
+        JSONObject data = new JSONObject(result.getData());
+        assertEquals("text", data.getString("content"));
     }
 
     @Test
     void parseXMLToJson_withNestedElements_returnsJsonWithNestedStructure() throws Exception {
         String xml = "<root><child><subchild>text</subchild></child></root>";
-        JSONObject json = Xml2Json.parse(xml);
-        if(debug){
-            System.out.println(json.toString(2));
+        String path = "root.child.subchild";
+
+        ArrayList<String> paths = new ArrayList<>();
+        paths.add(path);
+
+        ImportMappings mappings = new ImportMappings()
+                .data(paths);
+
+        ImportTemplate importTemplate = new ImportTemplate()
+                .metadata(new Metadata())
+                .data(mappings);
+
+        ImportedData result = Xml2Json.parse(xml, importTemplate);
+
+        if(debug) {
+            System.out.println("Result: " + result.toString());
         }
-        assertEquals("text", json.getJSONObject("root").getJSONObject("child").getJSONObject("subchild").getString("content"));
+
+        JSONObject data = new JSONObject(result.getData());
+        assertEquals("text", data.getJSONObject("subchild").getString("content"));
     }
 
     @Test
     void parseXMLToJson_withRepeatedElements_returnsJsonWithArray() throws Exception {
         String xml = "<root><child>text1</child><child>text2</child></root>";
-        JSONObject json = Xml2Json.parse(xml);
-        if(debug){
-            System.out.println(json.toString(2));
+        String path = "root.child";
+
+        ArrayList<String> paths = new ArrayList<>();
+        paths.add(path);
+
+        ImportMappings mappings = new ImportMappings()
+                .data(paths);
+
+        ImportTemplate importTemplate = new ImportTemplate()
+                .metadata(new Metadata())
+                .data(mappings);
+
+        ImportedData result = Xml2Json.parse(xml, importTemplate);
+
+        if(debug) {
+            System.out.println("Result: " + result.toString());
         }
-        JSONArray children = json.getJSONObject("root").getJSONArray("child");
+
+        JSONObject data = new JSONObject(result.getData());
+        JSONArray children = data.getJSONArray("child");
         assertEquals("text1", children.getJSONObject(0).getString("content"));
         assertEquals("text2", children.getJSONObject(1).getString("content"));
     }
@@ -69,8 +105,18 @@ class Xml2JsonTest {
     @Test
     void parseXMLToJson_withInvalidXml_throwsException() {
         String xml = "<root><child></root>";
-        assertThrows(SAXException.class, () -> {
-            Xml2Json.parse(xml);
-        });
+        String path = "root.child";
+
+        ArrayList<String> paths = new ArrayList<>();
+        paths.add(path);
+
+        ImportMappings mappings = new ImportMappings()
+                .data(paths);
+
+        ImportTemplate importTemplate = new ImportTemplate()
+                .metadata(new Metadata())
+                .data(mappings);
+
+        assertThrows(JSONException.class, () -> Xml2Json.parse(xml, importTemplate));
     }
 }
